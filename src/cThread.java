479c914aefb0e9189842ca9097c685cfa087e2a8
jozadaquebatista <jozadaquebatista@gmail.com>
2015-08-06 17:53:51 -0300

    Threads Threads Threads ...

--- src/cThread.java
+++ src/cThread.java
@@ -14,23 +14,33 @@ public class cThread extends Thread
       {
       
         for(int i = 0; i < 10; i++){
-          System.out.println("This is thread t");
               
           try
           {
+            System.out.println("This is thread t");
             Thread.sleep(300);
           }
           catch(InterruptedException e)
           {
             e.printStackTrace();
           }
-              
+          
+          if(i == 3)
+            break;
+          
         } 
             
       }
     
     };
     t.start();
+    for(int i = 0; i < 2; i++)
+    {
+    
+      
+    
+    }
+    
   }
 
 }
--- src/cThread3.java
+++ src/cThread3.java
@@ -1,5 +1,5 @@
 import java.lang.Thread;
-import java.util.*;
+import java.util.Random;
 
 class Tuna implements Runnable
 {
@@ -33,6 +33,8 @@ class Tuna implements Runnable
   
 }
 
+// MAIN CLASS
+
 public class cThread3
 {
 
@@ -43,11 +45,51 @@ public class cThread3
     Thread t2 = new Thread(new Tuna("two"));
     Thread t3 = new Thread(new Tuna("three"));
     Thread t4 = new Thread(new Tuna("four"));
+    Thread t5 = new Thread(new Tuna("five"));
+    Thread t6 = new Thread(new Tuna("six"));
+    Thread t7 = new Thread(new Tuna("seven"));
+    Thread t8 = new Thread(new Tuna("eight"));
+    Thread t9 = new Thread(new Tuna("nine"));
+    Thread t10 = new Thread(new Tuna("ten"));
+    Thread t11 = new Thread(new Tuna("eleven"));
+    Thread t12 = new Thread(new Tuna("twelve"));
+    Thread t13 = new Thread(new Tuna("thirteen"));
+    Thread t14 = new Thread(new Tuna("fourteen"));
+    Thread t15 = new Thread(new Tuna("fifteen"));
+    Thread t16 = new Thread(new Tuna("sixteen"));
+    Thread t17 = new Thread(new Tuna("seventeen"));
+    Thread t18 = new Thread(new Tuna("eighteen"));
+    Thread t19 = new Thread(new Tuna("nineteen"));
+    Thread t20 = new Thread(new Tuna("twenty"));
+
+    /* This doesn't Works D=
     
+    for(int i=1; i != 4; i++)
+    {
+    
+      t(i).start();
+    
+    }*/
     t1.start();
     t2.start();
     t3.start();
     t4.start();
+    t5.start();
+    t6.start();
+    t7.start();
+    t8.start();
+    t9.start();
+    t10.start();
+    t11.start();
+    t12.start();
+    t13.start();
+    t14.start();
+    t15.start();
+    t16.start();
+    t17.start();
+    t18.start();
+    t19.start();
+    t20.start();
     
   }
 
