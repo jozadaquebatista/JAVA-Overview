f55ff318ad4151057d173500c51872b8aedd0bdb
jozadaquebatista <jozadaquebatista@gmail.com>
2015-08-08 22:54:02 -0300

    Server

--- src/Chat/Server.java
+++ src/Chat/Server.java
@@ -2,29 +2,68 @@ import java.net.Socket;
 import java.net.ServerSocket;
 import java.util.Scanner;
 import java.io.IOException;
+import java.lang.StringBuffer;
+import java.lang.Thread;
 
 public class Server
 {
+  
+  Scanner reader;
 
   public Server()
   {
-  
+
     try {
     
       ServerSocket server = new ServerSocket(5500);
-      Scanner scanner = null;
       
-      while(scanner == null)
+      while(true)
       {
 
+        
         Socket socket = server.accept();
-        scanner = new Scanner(socket.getInputStream());
-        System.out.println(scanner.nextLine());
+        ListenClient task = new ListenClient(socket);
+        //new Thread(new ListenClient(socket)).start();
+        //scanner = new Scanner(socket.getInputStream());
+        //System.out.println(scanner.nextLine());
           
       }
           
-    } catch(IOException e) { }
+    } catch(IOException e){}
     
+  }
+
+  private class ListenClient implements Runnable
+  {
+  
+    public void ListenClient(Socket sckt)
+    {
+    
+      try {
+      
+        reader = new Scanner(sckt.getInputStream());        
+      
+      } catch(Exception e){}
+      
+    }
+  
+    public void run()
+    {
+    
+      try {
+      
+        String text;
+        while((text = reader.nextLine()) != null)
+        {
+        
+          System.out.println("Msg: " + text);
+        
+        }
+      
+      } catch(Exception x){}
+    
+    }
+  
   }
 
   public static void main(String[] args)
