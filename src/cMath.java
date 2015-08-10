cca62dd95ffb162bc764e3399127dc5761e7bc74
jozadaquebatista <jozadaquebatista@gmail.com>
2015-08-10 00:00:18 -0300

    lang.Math

--- src/cMath.java
+++ src/cMath.java
@@ -12,6 +12,8 @@ public class cMath
      
     // Example: that function returns wich number is big
     System.out.println(Math.max(2, 10));
+    // And this returns the square of a double number
+    System.out.println(Math.sqrt(25.0));
   
   }
 
