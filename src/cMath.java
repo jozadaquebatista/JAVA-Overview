e589fa5bb45da460fcbaa5628086ed10c9750f3a
jozadaquebatista <jozadaquebatista@gmail.com>
2015-08-09 23:58:40 -0300

    lang.Math

---
+++ src/cMath.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+
+public class cMath
+{
+
+  public static void main(String[] args)
+  {
+  
+    /*  The class lang.Math has the essensials methods to 
+    *   work with Math operations.
+     */
+     
+    // Example: that function returns wich number is big
+    System.out.println(Math.max(2, 10));
+  
+  }
+
+}
