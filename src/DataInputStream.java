acb88426e0d0ef17101b69ef2ee3213b6fc3a08a
jozadaquebatista <jozadaquebatista@gmail.com>
2015-08-10 23:55:03 -0300

    DataInputStream

--- src/DataInputStream.java
+++ src/DataInputStream.java
@@ -1,58 +1,35 @@
-    /*
-      Read boolean from file using DataInputStream
-      This Java example shows how to read a Java boolean primitive value from file using
-      readBoolean method of Java DataInputStream class.
-    */
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
      
-    import java.io.DataInputStream;
-    import java.io.FileInputStream;
-    import java.io.FileNotFoundException;
-    import java.io.IOException;
+public class ReadBooleanFromFile {
      
-    public class ReadBooleanFromFile {
+  public static void main(String[] args) {
      
-      public static void main(String[] args) {
-     
-        String strFilePath = "C://FileIO//readBoolean.txt";
+    String strFilePath = "C://FileIO//readBoolean.txt";
        
-        try
-        {
-          //create FileInputStream object
-          FileInputStream fin = new FileInputStream(strFilePath);
+    try
+    {
+      FileInputStream fin = new FileInputStream(strFilePath);
          
-          /*
-           * To create DataInputStream object, use
-           * DataInputStream(InputStream in) constructor.
-           */
+      DataInputStream din = new DataInputStream(fin);
          
-           DataInputStream din = new DataInputStream(fin);
-         
-           /*
-            * To read a Java boolean primitive from file, use
-            * byte readBoolean() method of Java DataInputStream class.
-            *
-            * This method reads one byte from file and returns true if byte is nonzero,
-            * false if the byte is zero.
-            */
+      boolean b = din.readBoolean();
            
-            boolean b = din.readBoolean();
+      System.out.println("boolean : " + b);
            
-            System.out.println("boolean : " + b);
+
+      din.close();
            
-            /*
-             * To close DataInputStream, use
-             * void close() method.
-             */
-             din.close();
-           
-        }
-        catch(FileNotFoundException fe)
-        {
-          System.out.println("FileNotFoundException : " + fe);
-        }
-        catch(IOException ioe)
-        {
-          System.out.println("IOException : " + ioe);
-        }
-      }
     }
+    catch(FileNotFoundException fe)
+    {
+      System.out.println("FileNotFoundException : " + fe);
+    }
+    catch(IOException ioe)
+    {
+      System.out.println("IOException : " + ioe);
+    }
+  }
+}
