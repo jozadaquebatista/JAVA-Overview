1a4bb132cb6b2459957ad11090ff10ffbf33ac20
jozadaquebatista <jozadaquebatista@gmail.com>
2015-08-11 15:03:27 -0300

    cLoops

---
+++ src/cLoops.java
@@ -0,0 +1,51 @@
+class Recursive
+{
+  public Recursive();
+  
+  static int Looping(int i)
+  {
+    i++;
+      
+    System.out.println("I'm the var 'i'=" + i + ", and I'm inside the recursive function.");
+      
+    if(i != 10)
+      return Looping(i);
+    else
+      return 0;
+  }
+  
+  public static void main(String[] args){}
+}
+
+public class cLoops
+{
+
+  public static void main(String[] args)
+  {
+    /* We have three type of standard loop statements = FOR, WHILE and DO-WHILE*/
+    
+    //example
+    while(true)
+    {
+      for(int i=0; i<10; i++)
+      {
+        do {
+          System.out.println("value of 'i' now is = " + i);
+        } while(i == 10);
+      }
+      break; // When var i >= 10 break all statements.
+    }
+  }
+  
+  /*
+  *  Other way to get this same effect, is by using recursive functions
+  *  However this way probably can give us a stack overflow.
+   */
+
+    // we'll use this variable soon.
+    int varloop = 0;
+    
+    Recursive r = new Recursive();
+    r.Looping(varloop); // executes a recursive function
+    
+}
