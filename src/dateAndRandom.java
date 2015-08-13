432e98e4ae43b7f3371c1a6b26265b9de7144942
jozadaquebatista <jozadaquebatista@gmail.com>
2015-08-12 23:22:01 -0300

    dateAndRandomNumber

---
+++ src/dateAndRandom.java
@@ -0,0 +1,19 @@
+
+import java.util.Date;
+import java.lang.Math;
+
+public class dateAndRandom
+{
+
+  public static void main(String[] args)
+  {
+  
+    // Show us the current date.
+    System.out.println(new Date());
+    
+    // Show us a random number
+    System.out.println(Math.random());
+    
+  }
+
+}
