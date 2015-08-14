2b5a9048b6ab2837985c4c63457946150c676892
Jozadaque Batista <jozadaquebatista@gmail.com>
2015-08-14 13:07:56 -0300

    Race challenge main class

---
+++ src/Race/raceChallenge.java
@@ -0,0 +1,15 @@
+
+
+public class raceChallenge
+{
+
+  public static void main(String[] args)
+  {
+    
+    new Runner(1,0).start();
+    new Runner(2,0).start();
+    new Runner(3,0).start();
+        
+  }
+  
+}
