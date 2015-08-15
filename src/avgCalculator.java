07c7d7d0a52ff2c2c4cad0aaeb5d6f09c8c09dd5
Jozadaque Batista <jozadaquebatista@gmail.com>
2015-08-15 18:22:35 -0300

    avgCalculator

---
+++ src/avgCalculator.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
+
+
+public class avgCalculator
+{
+
+  public static void main(String[] args)
+  {
+  
+    System.out.println("#### Avg ####" + "\n");
+    
+    String name;
+    List<Integer> avgTime = new ArrayList<>();
+    
+  
+  }
+
+}
