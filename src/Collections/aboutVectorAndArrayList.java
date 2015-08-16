bbeb58270b656ba7721b19e0ecc328eafb415fc9
Jozadaque Batista <jozadaquebatista@gmail.com>
2015-08-16 17:40:22 -0300

    aboutVectorAndArrayList up

--- src/Collections/aboutVectorAndArrayList.java
+++ src/Collections/aboutVectorAndArrayList.java
@@ -1,4 +1,6 @@
 import java.util.Vector;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class aboutVectorAndArrayList
 {
@@ -6,6 +8,7 @@ public class aboutVectorAndArrayList
   public static void main(String[] args)
   {
   
+    /* Generics they are important, we want a Vector of String, we don't like casting!*/
     Vector<String> names = new Vector<String>(3); // This means that vector will be initializes with 3 positions
                                   // However the vector will expand always
                                   
@@ -18,6 +21,35 @@ public class aboutVectorAndArrayList
     names.add("Paula");
     
     
+    // There is two ways of print all elements
+    System.out.println(names);
+    System.out.println(names.toString());
+    
+    // Here I created an ArrayList that will contain 10 positions initially
+    ArrayList<String> other = new ArrayList<String>(10);
+    
+    other.add("Claudio");
+    other.add("Sthefanny");
+    other.add("Simao");
+    other.add("Manoela");
+    other.add("Victor");
+    
+    // There is two ways of print all elements
+    System.out.println(other);
+    System.out.println(other.toString());
+  
+    // So now let's play with some interator to show this elements
+    
+    Iterator<String> it = names.iterator();
+    
+    while(it.hasNext())
+    {
+      
+      String tmp = new String(it.next());
+    
+      System.out.println("[" + names.indexOf(tmp) + "] name in array is: " + tmp);
+    
+    }
   
   }
 
