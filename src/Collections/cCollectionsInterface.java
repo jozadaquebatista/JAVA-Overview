b4b695f5a53bdc247dd9f58b51813d33ea1ad181
Jozadaque Batista <jozadaquebatista@gmail.com>
2015-08-15 23:56:44 -0300

    About Collections

---
+++ src/Collections/cCollectionsInterface.java
@@ -0,0 +1,35 @@
+import java.util.Set;
+import java.util.List;
+import java.util.Queue;
+import java.util.Map;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.HashMap;
+
+
+public class cCollectionsInterface
+{
+
+  public static void main(String[] args)
+  {
+  
+    /* In collections we have tree types that are: */
+  
+    Set<String> set = new LinkedHashSet<>();
+    List<String> list = new LinkedList<>();
+    Queue<String> queue = new LinkedList<>();
+    
+    /* Map doesnt make part of Collections framework, keep it in mind. */
+    
+    Map<String, String> map = new HashMap<>();
+    
+    /* For all that, we have seem methods 
+    *  add()
+    *  remove()
+    *  offer()
+    *
+    *  But I'll review that*/
+  
+  }
+
+}
