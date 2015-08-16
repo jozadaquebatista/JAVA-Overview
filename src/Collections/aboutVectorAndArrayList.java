a4ab7d2146509227370743187949fc93be36e37a
Jozadaque Batista <jozadaquebatista@gmail.com>
2015-08-16 17:13:07 -0300

    Playing with Vector and ArrayLists

---
+++ src/Collections/aboutVectorAndArrayList.java
@@ -0,0 +1,24 @@
+import java.util.Vector;
+
+public class aboutVectorAndArrayList
+{
+
+  public static void main(String[] args)
+  {
+  
+    Vector<String> names = new Vector<String>(3); // This means that vector will be initializes with 3 positions
+                                  // However the vector will expand always
+                                  
+    names.add("Estevão");
+    names.add("Jozadaque");
+    names.add("Marlom");
+    names.add("Joaquim");
+    names.add("Lucas");
+    names.add("Roberta");
+    names.add("Paula");
+    
+    
+  
+  }
+
+}
