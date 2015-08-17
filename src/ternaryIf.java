2921320606bef865f919de63135907efb332b0f5
Jozadaque Batista <jozadaquebatista@gmail.com>
2015-08-16 22:19:08 -0300

    Working with & and |

---
+++ src/ternaryIf.java
@@ -0,0 +1,50 @@
+
+public class ternaryIf
+{
+
+  public static void main(String[] args)
+  {
+  
+    int a = 5;
+    int b = 16;
+    
+    if(a > 6 && b > 12)
+    {
+    
+      // execute foo ...
+    
+    }
+    
+    if(a > 6 || b > 12)
+    {
+    
+      // execute foo ...
+    
+    }
+    
+    /* && and || look if at least one of the sentences are true. */
+    
+    if(a > 6 & b > 12)
+    {
+    
+      // execute foo ...
+    
+    }
+    
+    if(a > 6 | b > 12)
+    {
+    
+      // execute foo ...
+    
+    }
+    
+    /* & and | look if both sentences are true.
+    *
+    *  That's all.
+    *
+     */
+      
+  
+  }
+
+}
