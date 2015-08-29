64de3135f42e0a0f033483e97a8d5bd9d51097d0
jozadaquebatista <jozadaquebatista@gmail.com>
2015-08-29 02:54:20 -0300

    threadAndRunnable with issues

--- src/Threads/threadAndRunnable.java
+++ src/Threads/threadAndRunnable.java
@@ -1,31 +1,39 @@
 import java.lang.Thread;
 import java.lang.StringBuilder;
+import java.util.Random;
 
 public class threadAndRunnable extends Thread implements Runnable
 {
 
     private StringBuilder msg;
 
+    public threadAndRunnable(){}
     public threadAndRunnable(StringBuilder msg){ this.msg = msg; }
 
     public void run()
     {
+	// Generates a random number
+	Random random_n = new Random();
 
 	for(int i=0; i < 10; i++)
         {
 	    System.out.println("olá!");
-	    try{Thread.sleep(200);}catch(InterruptedException e){}         
+	    try{Thread.sleep(random_n.nextInt(200));}catch(InterruptedException e){}         
         }
 
     }
 
     public void run(StringBuilder msg)
     {
+	// Generates a random number
+	Random random_n = new Random();
+
+	msg = this.msg;
 
 	for(int i=0; i < 10; i++)
         {
-	    System.out.println(msg);
-	    try{Thread.sleep(200);}catch(InterruptedException e){}         
+	    System.out.println("Hahaha");
+	    try{Thread.sleep(random_n.nextInt(200));}catch(InterruptedException e){}         
         }
 
     }
@@ -33,11 +41,11 @@ public class threadAndRunnable extends Thread implements Runnable
     public static void main(String[] args)
     {
 
+	StringBuilder s = new StringBuilder("Hahaha");
 
 	// Notice: void run method are the same to both, but I overloaded it.
-
-	Thread t = new Thread(new threadAndRunnable(new StringBuilder("Hahaha"))); // this calls void run method 
-	t.start();                                      // by the Interface Runnable
+	Thread t = new Thread(new threadAndRunnable(s)); // this calls void run method 
+	t.start();                                       // by the Interface Runnable
 
 	(new threadAndRunnable()).start(); // this calls a Thread by class Thread()
 
