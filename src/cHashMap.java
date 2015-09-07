a5d006123a37c01a86c4ea1608d734277e846280
jozadaquebatista <jozadaquebatista@gmail.com>
2015-09-06 23:13:37 -0300

    HashMap Class very Useful

---
+++ src/cHashMap.java
@@ -0,0 +1,34 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/*
+*
+*  HASHMAP is a datastructer thats works with K, V(key and value)
+*
+ */
+
+public class cHashMap
+{
+
+    public static void main(String[] args)
+    {
+    
+        Map<Integer, String> clients = new HashMap<>();
+        
+        clients.put(1, "Jozadaque");
+        clients.put(2, "Stella");
+        clients.put(3, "Marcos");
+        clients.put(4, "Marina");
+        clients.put(5, "Carlos");
+        
+        // This doesnt work, I still know only how to put in a HashMap
+        /**for(Map.Entry entry : codes.entrySet())
+        {
+            reversedMap.put(entry.getValue(), entry.getKey());
+        }
+        System.out.println(clients.);**/
+    
+    }
+
+}
