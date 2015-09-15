c70b53d29e23bd09dafee7e6f00289040c8f81cd
Jozadaque Batista <jozadaquebatista@gmail.com>
2015-09-14 23:33:45 -0300

    Working with files

---
+++ src/WorkingWithFiles.java
@@ -0,0 +1,33 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyBytes {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+          
+          /*
+          * This program catches content a file and write over another file.
+           */
+        
+            in = new FileInputStream("inputFileTest.txt");
+            out = new FileOutputStream("outputFileTest.txt");
+            int c;
+
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+}
