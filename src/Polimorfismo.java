ad7c8561802e246dfff15d8ff0cb9ff80a19ef68
Jozadaque Batista <jozadaquebatista@gmail.com>
2015-09-18 23:44:38 -0300

    Trying to understand polymorphism =/

---
+++ src/Polimorfismo.java
@@ -0,0 +1,21 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.LinkedList;
+
+
+
+public class Polimorfismo
+{
+
+    public static void main(String[] args)
+    {
+    
+        HashMap<Integer, String> users_table = new HashMap<Integer, String>();
+        
+        List users_table tbl = new LinkedList<>();
+        
+    
+    }
+
+}
