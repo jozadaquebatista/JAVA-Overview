973fa07301f79605e3a1977f7361a9d9acfd98c8
Jozadaque Batista <jozadaquebatista@gmail.com>
2015-09-24 22:52:07 -0300

    Simple Quick Sort in JAVA

---
+++ src/QuickSort.java
@@ -0,0 +1,32 @@
+
+
+public class QuickSort {
+     
+    private int array[];
+    private int length;
+ 
+    public void sort(int[] inputArr) {
+         
+        if (inputArr == null || inputArr.length == 0) {
+            return;
+        }
+        this.array = inputArr;
+        length = inputArr.length;
+        quickSort(0, length - 1);
+    }
+ 
+    private void quickSort(int lowerIndex, int higherIndex) {
+         
+        
+     
+    public static void main(String a[]){
+         
+        MyQuickSort sorter = new MyQuickSort();
+        int[] input = {24,2,45,20,56,75,2,56,99,53,12};
+        sorter.sort(input);
+        for(int i:input){
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+}
